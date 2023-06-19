98e67f514e8f73c04c071a80eff903111dfced78
Oksana Komarnytska <okazimiriv@gmail.com>
2023-06-19 22:28:48 +0200

    instale

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
