de189617722839c7272b74c2a042e8612f426b4e
Oksana Komarnytska <okazimiriv@gmail.com>
2023-06-30 02:24:08 +0200

    fix11

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 import { Wrapper, Input, Icon, Button } from './SearchForm.styled';
-import 'react-toastify/dist/ReactToastify.min.css';
 
 const SearchForm = ({ value, onChange, onSubmit }) => {
   return (
     <Wrapper onSubmit={onSubmit}>
-      <Icon />
       <Input
         type="text"
         // autoComplete="off"
